ea53ee46e027d7407367a2be1fa724287d6b799c
SAGAR_J0 <24331a05j0@mvgrce.edu.in>
2025-12-29 19:03:45 +0530

    Add files via upload

---
+++ 2D.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr1=np.array([[1,2,3],
+               [4,5,6]])
+arr2=np.array([[7,8,9],
+               [10,11,12]])
+print(arr1)
+print(arr2)
+print("Addition of two arrays:",arr1+arr2)
+print("Substraction of two arrays:",arr1-arr2)
+print("Multiplication of two arrays:",arr1*arr2)
+print("Division of two arrays:",arr1/arr2)
